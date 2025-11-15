8c86e01043f610f293cabf4c1f3efab5c64c6599
Camilla Midori Kanashiro Santos <camilla.santos8@fatec.sp.gov.br>
2025-11-15 13:19:07 -0300

    Add files via upload

---
+++ scripts/script-comunidade.js
@@ -0,0 +1,11 @@
+function participarComunidade(){
+    console.log("Sucesso!");
+    function alertaParticiparComunidade(){
+        Swal.fire({
+        title: "Sucesso!",
+        text: "Agora você está participando desta comunidade.",
+        icon: "success"
+        });
+    }
+    alertaParticiparComunidade();
+}
